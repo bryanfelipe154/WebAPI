3151641e821a8c52b681f15b1d5c1b95ba5b79d1
oFelipe27 <felipe.bryan995@gmail.com>
2023-01-14 12:05:54 -0300

    added first infra configuration

---
+++ WebAPI.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebAPI.Infra.Data.Context;
+
+namespace WebAPI.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                options.UseSqlServer(connectionString);
+            });
+
+            return services;
+        }
+    }
+}
