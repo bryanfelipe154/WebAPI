5197767cdd2a2a038b9e79c9d415396eb60983b9
oFelipe27 <felipe.bryan995@gmail.com>
2023-01-14 11:10:21 -0300

    added first interfaces of domains

---
+++ WebAPI.Domain/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace WebAPI.Domain.Interfaces
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        Task<T> GetByIdAsync(Func<T, bool> predicate);
+        Task<IEnumerable<T>> FindAllAsync();
+        Task<T?> FindByIdAsync(int id);
+        Task<T?> AddAsync(T obj);
+        Task<T> UpdateAsync(T obj);
+        Task<bool> DeleteAsync(int id);
+    }
+}
