d395c3211ce55cd806a58cb5532f8de30158889f
oFelipe27 <felipe.bryan995@gmail.com>
2023-01-14 10:45:41 -0300

    added first entities

---
+++ WebAPI.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Domain.Entities
+{
+    public sealed class Product : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+    }
+}
