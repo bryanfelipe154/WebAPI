0c8aae009d3370dcbcc2a984bf5fedf734b6d25d
oFelipe27 <felipe.bryan995@gmail.com>
2023-01-15 21:08:42 -0300

    added identity pack

--- WebAPI.Infra.Data/Context/ApplicationDbContext.cs
+++ WebAPI.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WebAPI.Domain.Entities;
 
 namespace WebAPI.Infra.Data.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
